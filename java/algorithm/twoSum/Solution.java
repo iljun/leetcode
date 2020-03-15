29896b445afdd15b95ed32774e4493ab67068ccf
iljun <sumusb@naver.com>
2020-03-15 12:50:48 +0900

    twoSum

---
+++ java/algorithm/twoSum/Solution.java
@@ -0,0 +1,22 @@
+package algorithm.twoSum;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        new Solution().twoSum(new int[]{2, 7, 11, 15}, 9);
+    }
+
+    public int[] twoSum(int[] nums, int target) {
+        int[] result = new int[2];
+        a : for (int i = 0 ; i < nums.length - 1; i++) {
+            result[0] = i;
+            for (int j = i + 1 ; j < nums.length; j++) {
+                if (nums[i] + nums[j] == target) {
+                    result[1] = j;
+                    break a;
+                }
+            }
+        }
+        return result;
+    }
+}
