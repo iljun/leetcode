5eb706f96abc29eb65b6295b2f20921d65a8b164
iljun <sumusb@naver.com>
2020-03-20 16:07:27 +0900

    longestSubstringNoRepeated

---
+++ java/algorithm/LongestSubstringWithoutRepeatingCharacters/Solution.java
@@ -0,0 +1,27 @@
+package algorithm.LongestSubstringWithoutRepeatingCharacters;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        System.out.println(new Solution().lengthOfLongestSubstring("abcabcbb"));
+    }
+
+    public int lengthOfLongestSubstring(String s) {
+        int n = s.length();
+        int result = 0;
+        int i = 0;
+        Map<Character, Integer> map = new HashMap<>();
+        for (int j = 0; j < n; j++) {
+            if (map.containsKey(s.charAt(j))) {
+                i = Math.max(map.get(s.charAt(j)), i);
+            }
+            result = Math.max(result, j - i + 1);
+            map.put(s.charAt(j), j + 1);
+        }
+        return result;
+    }
+
+}
