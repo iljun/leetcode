2e3d63a0bb1173dfd1ef2f1cf349cee5b33d6ea3
iljun <sumusb@naver.com>
2020-03-24 22:54:03 +0900

    container with most water

---
+++ java/algorithm/containerWithMostWater/Solution.java
@@ -0,0 +1,23 @@
+package algorithm.containerWithMostWater;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        int[] height = new int[]{1,8,6,2,5,4,8,3,7};
+        System.out.println(new Solution().maxArea(height));
+    }
+
+    public int maxArea(int[] height) {
+        int maxArea = 0;
+
+        for (int i = 0; i < height.length - 1; i++) {
+            for (int j = i + 1; j < height.length; j++) {
+                int horizontal = j - i;
+                int vertical = Math.min(height[i], height[j]);
+                maxArea = Math.max(maxArea, horizontal * vertical);
+            }
+        }
+
+        return maxArea;
+    }
+}
